071ea514261ef315e3041e2f7e4d26d11da601f3
Amol94 <amolbiradar16@gmail.com>
2020-06-11 23:29:37 +0530

    new code

--- smoothing_images_and_image_blurring.py
+++ smoothing_images_and_image_blurring.py
@@ -2,18 +2,17 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#img = cv2.imread('opencv-logo.png')
-#img = cv2.imread('Noise_salt_and_pepper.png')
+# img = cv2.imread('opencv-logo.png')
+# img = cv2.imread('Noise_salt_and_pepper.png')
 img = cv2.imread('lena.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-kernal = np.ones((5, 5), np.float32)/25
+kernal = np.ones((5, 5), np.float32) / 25
 dst = cv2.filter2D(img, -1, kernal)
 blur = cv2.blur(img, (5, 5))
 gblure = cv2.GaussianBlur(img, (5, 5), 0)
 median = cv2.medianBlur(img, 5)
 bilaterlFilter = cv2.bilateralFilter(img, 9, 75, 75)
-
 
 titles = ['image', '2D convolution', 'blure', 'Gaussianblure', 'median', 'bilaterlFilter']
 images = [img, dst, blur, gblure, median, bilaterlFilter]
