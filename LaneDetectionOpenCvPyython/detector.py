18b0010ecb45e9d870a6968369391fbfccd54127
Amol94 <amolbiradar16@gmail.com>
2020-06-18 19:53:21 +0530

    new code

--- LaneDetectionOpenCvPyython/detector.py
+++ LaneDetectionOpenCvPyython/detector.py
@@ -2,19 +2,6 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-image = cv2.imread('road1.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-print(image.shape)
-height = image.shape[0]
-width = image.shape[1]
-
-region_of_interest_vertices = [
-    (0, height),
-    (width / 2, height / 2),
-    (width, height)
-]
-
 
 def region_of_interest(img, vertices):
     mask = np.zeros_like(img)
@@ -37,17 +24,46 @@ def drow_the_lines(img, lines1):
     return img
 
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-canny_image = cv2.Canny(gray_image, 100, 200)
-cropped_image = region_of_interest(canny_image,
-                                   np.array([region_of_interest_vertices], np.int32))
-lines = cv2.HoughLinesP(cropped_image,
-                        rho=6,
-                        theta=np.pi / 60,
-                        threshold=160,
-                        lines=np.array([]),
-                        minLineLength=40,
-                        maxLineGap=25)
-image_with_line = drow_the_lines(image, lines)
-plt.imshow(image_with_line)
-plt.show()
+# image = cv2.imread('road1.png')
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def process(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+
+    region_of_interest_vertices = [
+        (0, height),
+        (width / 2, height / 2),
+        (width, height)
+    ]
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    canny_image = cv2.Canny(gray_image, 100, 120)
+    cropped_image = region_of_interest(canny_image,
+                                       np.array([region_of_interest_vertices], np.int32), )
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=2,
+                            theta=np.pi / 180,
+                            threshold=50,
+                            lines=np.array([]),
+                            minLineLength=40,
+                            maxLineGap=100)
+
+    image_with_line = drow_the_lines(image, lines)
+    return image_with_line
+
+
+cap = cv2.VideoCapture('test2.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    frame = process(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+# plt.imshow(image_with_line)
+# plt.show()
